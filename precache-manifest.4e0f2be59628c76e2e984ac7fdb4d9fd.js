c0b20718d59c798f8383b67497d023db4e5397ef
JavierAle <javierale25@live.com>
2020-07-30 11:05:19 -0300

    Updates

---
+++ precache-manifest.4e0f2be59628c76e2e984ac7fdb4d9fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f18367c5e61ecfe48be3732965e4d8c",
+    "url": "/turnosVet/index.html"
+  },
+  {
+    "revision": "b387f6fa93690950eb7b",
+    "url": "/turnosVet/static/css/main.33116e20.chunk.css"
+  },
+  {
+    "revision": "e6c1c108bfaaed79ba29",
+    "url": "/turnosVet/static/js/2.bf470eb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/turnosVet/static/js/2.bf470eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b387f6fa93690950eb7b",
+    "url": "/turnosVet/static/js/main.b08359d7.chunk.js"
+  },
+  {
+    "revision": "4001266715ccb0f601d7",
+    "url": "/turnosVet/static/js/runtime-main.c349f9f9.js"
+  }
+]);
